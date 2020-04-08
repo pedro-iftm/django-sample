4a46115e4a6228a76a53d380ecd57723d5035760
Pedro Santos Arruda <pedro.arruda@morpheustecnologia.com>
2020-04-07 22:37:43 -0300

    Merge missing version

--- accounts/forms.py
+++ accounts/forms.py
@@ -23,3 +23,10 @@ class RegisterForm(UserCreationForm):
             user.save()
 
         return user
+
+
+class EditAccount(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
